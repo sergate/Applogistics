27ac5bb42af7e8e1e9120ec494b8b247f40908a5
sergate <104356466+sergate@users.noreply.github.com>
2026-07-13 14:45:35 -0300

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   output: process.env.NEXT_OUTPUT_MODE,
   productionBrowserSourceMaps: false,
   experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
+    outputFileTracingRoot: undefined,
   },
   eslint: {
     ignoreDuringBuilds: true,
